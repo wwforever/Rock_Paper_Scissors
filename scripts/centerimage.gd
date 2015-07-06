ef91777b286c12322404e1539f29af9b5a1ce209
wwforever <wwforever80@gmail.com>
2015-07-06 21:45:09 +0900

    가위 바위 보 빠른 전개

--- scripts/centerimage.gd
+++ scripts/centerimage.gd
@@ -5,6 +5,9 @@ extends Node2D
 # var a=2
 # var b="textvar"
 var images = []
+var showIndex = 0
+var showTime = 0.05
+var lastTime = 0.0
 
 func _ready():
 	# Initialization here
@@ -14,7 +17,19 @@ func _ready():
 	images[2] = get_node("Paper")
 	
 	for i in range(3):
-		print(images[i].get_name())	
+		if i != showIndex:
+			images[i].hide()
+			
+	set_process(true)
 	
+func _process(delta):
+	lastTime += delta
+	if (lastTime >= showTime):
+		images[showIndex].hide()
+		showIndex += 1
+		lastTime = 0
+	
+	if (showIndex >= images.size()):
+		showIndex = 0
 
-
+	images[showIndex].show()
